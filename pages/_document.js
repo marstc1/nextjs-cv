afc665a5f1d5abfdfb6c321a1b4ca1fe38bfa9d7
Chris Marston <christopher.marston@hotmail.co.uk>
2021-08-11 18:06:21 +0100

    Hosting fonts statically

--- pages/_document.js
+++ pages/_document.js
@@ -17,11 +17,6 @@ class MyDocument extends Document {
           />
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width" />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
